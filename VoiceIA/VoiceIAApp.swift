cb67fb4ace842cbe73a4e18a78451958a6c993ff
Arley Santana <dev.arley.santana@gmail.com>
2026-08-08 14:26:16 -0300

    Initial Commit

---
+++ VoiceIA/VoiceIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceIAApp.swift
+//  VoiceIA
+//
+//  Created by Arley Pereira Santana on 08/08/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
